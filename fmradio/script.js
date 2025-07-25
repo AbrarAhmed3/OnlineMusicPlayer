e2dfb95b9ce8648cc130779d7cff27f7a35a5a03
Abrar Ahmed <109240832+AbrarAhmed3@users.noreply.github.com>
2025-07-25 21:56:48 +0530

    disabled right clicks

--- fmradio/script.js
+++ fmradio/script.js
@@ -268,3 +268,21 @@
   event_label: station.name,
   value: 1
 });
+
+
+
+// dissable right clicks and shortcuts
+document.addEventListener("contextmenu", function (e) {
+  e.preventDefault();
+});
+
+document.addEventListener("keydown", function (e) {
+  if (
+    e.key === "F12" ||
+    (e.ctrlKey && e.shiftKey && (e.key === "I" || e.key === "C" || e.key === "J")) ||
+    (e.ctrlKey && e.key === "U")
+  ) {
+    e.preventDefault();
+    alert("Inspecting is disabled!");
+  }
+});
